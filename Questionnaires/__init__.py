8bc649eba7650051b172399da47d3ba9dcee22fe
aguenth <anne.guenther@unibas.ch>
2024-06-28 11:57:42 +0200

    changed modal warning

--- Questionnaires/__init__.py
+++ Questionnaires/__init__.py
@@ -20,28 +20,12 @@ class Player(BasePlayer):
         choices=['0', '1', '2', '3', '4', '5', '7', '8'],
         label="How many spontaneous trips do you typically make in one week in addition to the planned trips recorded in your trip diary?")
 
-    enough_options = models.StringField(
-        choices=['No', 'Yes, I wanted to indicate more trips than were possible.'],
-        widget=widgets.RadioSelect,
-        label="Did you encounter any problems where you wanted to indicate more trips on a day than were possible?"
-    )
-
-    general_comment = models.LongStringField(
-        label="Please describe any problems or difficulties you had while filling in this survey. Your feedback will help us improve it."
-    )
-
 
 class Spontaneous(Page):
     form_model = 'player'
     form_fields = ['spontaneous_trips']
 
 
-class Comments(Page):
-    form_model = 'player'
-    form_fields = ['enough_options', 'general_comment']
-
-
 page_sequence = [
-    Spontaneous,
-    Comments
+    Spontaneous
 ]
