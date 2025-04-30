6304cfca9c7e9e63f0a90c13e552f3e798041d55
Nina Frings <nina.frings@unibas.ch>
2025-04-30 11:43:14 +0200

    field for de

--- Intro_t1_de/__init__.py
+++ Intro_t1_de/__init__.py
@@ -24,6 +24,8 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
+    quota_full = models.IntegerField(initial=0)
+
     def make_field(label):
         return models.IntegerField(
         choices=[1,2,3,4,5,6,7,8,9,10],                           
